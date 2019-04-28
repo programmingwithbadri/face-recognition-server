9d91667b2d310774490f5727661a31357716cbf9
Badri <badri.py@outlook.com>
2019-04-28 19:47:58 +0530

    Create the API for signin with mock value

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
+const database = {
+    users:
+        [{
+            id: 1,
+            name: 'John',
+            email: 'john@gmail.com',
+            password: '12345',
+            entries: 1,
+            joined: new Date()
+        },
+        {
+            id: 2,
+            name: 'James',
+            email: 'james@gmail.com',
+            password: '54321',
+            entries: 2,
+            joined: new Date()
+        }]
+};
+app.get('/', (req, res) => {
     res.send('This is working');
 });
 
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.status('200').json("Success")
+    }
+    else {
+        res.status('400').json("Bad Request")
+    }
+});
+
 app.listen(3001, () => {
-    console.log("Server is up"); 
+    console.log("Server is up");
 });

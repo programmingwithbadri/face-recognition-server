83385fbcf7dc3e5f48e1824934e35ac5bbf2ba75
Badri <badri.py@outlook.com>
2019-05-04 23:00:41 +0530

    Update the register method to add data to login table

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -53,13 +54,26 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    db('users')
-    .returning('*')
-    .insert({
-        name: name,
-        email: email,
-        joined: new Date()
-    }).then(user => res.json(user[0]))
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            email: email,
+            hash: hash
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                name: name,
+                email: loginEmail[0],
+                joined: new Date()
+            })
+            .then(user => res.json(user[0]))
+        }).then(trx.commit)
+        .catch(trx.rollback)
+    })
     .catch(() => res.status('400').json('Unable to register'))
 })
 
